f3a22d94821b46d8aa7bbd30b20698f4215dac8d
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-11-04 20:03:00 +0000

    Work in progress.

--- src/DappCampNFT.sol
+++ src/DappCampNFT.sol
@@ -11,22 +11,50 @@ contract DappCampNFT is ERC721Enumerable, Ownable {
 
     constructor() ERC721("DappCamp NFT", "DCAMP") Ownable() {}
 
-    string[] private collection = [""];
+    string[] private collection = [
+        "Maverick", "Iceman", "Goose", "Rooster", "Viper", "Hangman", "Phoenix", "Cougar", "Bob", "Hollywood"
+    ];
 
-    function random(string memory input) internal pure returns (uint256) {}
+    string[] private songs = [
+        "The Tracks of My Tears", 
+        "Danger Zone",
+        "I Ain't Worried", 
+        "(Sittin' On) The Dock of the Bay",
+        "Great Balls of Fire",
+        "Broken Wings",
+        "The Final Countdown"
+    ];
 
-    function pluck(
-        uint256 tokenId,
-        string memory keyPrefix,
-        string[] memory sourceArray
-    ) internal pure returns (string memory) {}
+    function random(string memory input) internal pure returns (uint256) {
+        return uint(keccak256(bytes(input)));        
+    }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override
-        returns (string memory)
-    {}
+    function pluck(uint256 tokenId, string memory keyPrefix, string[] memory sourceArray) internal pure returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked(keyPrefix, Strings.toString(tokenId))));    
+        string memory output = sourceArray[rand % sourceArray.length];
+        return output;
+    }
+
+    function tokenURI(uint256 tokenId) override public view returns (string memory) {
+        string[5] memory parts;
+        parts[0] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill="black" /><text x="10" y="20" class="base">';
+        parts[1] = getCallSign(tokenId);
+        parts[2] = '</text><text x="10" y="40" class="base">';
+        parts[3] = getFavouriteSong(tokenId);
+        parts[4] = '</text></svg>';
+        string memory output = string(abi.encodePacked(parts[0], parts[1], parts[2], parts[3], parts[4]));
+        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": parts[1], "song": parts[2], "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
+        output = string(abi.encodePacked('data:application/json;base64,', json));
+        return output;
+    }
+
+    function getCallSign(uint256 tokenId) public view returns (string memory) {
+        return pluck(tokenId, "CALLSIGN", collection);
+    }
+
+    function getFavouriteSong(uint256 tokenId) public view returns (string memory) {
+        return pluck(tokenId, "SONG", songs);
+    }
 
     function claim(uint256 tokenId) public {
         require(
